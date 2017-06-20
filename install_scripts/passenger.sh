d8e52f382d444737b85a815dbfcaa611bff5e930
Sean Watkins <slwatkins@uh.edu>
2017-06-20 08:48:12 -0500

    Fix ruby install

--- install_scripts/passenger.sh
+++ install_scripts/passenger.sh
@@ -12,10 +12,10 @@ fi
 
 su - avalon << EOF
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -q -L https://get.rvm.io | bash -s stable --ruby=2.2.5
+curl -sSL https://get.rvm.io | bash -s stable
 source /home/avalon/.rvm/scripts/rvm
 
-rvm use 2.2.5
+rvm install 2.2.5
 rvm  --default use 2.2.5
 
 echo "Installing passenger..."
