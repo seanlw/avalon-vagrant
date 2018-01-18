92dc1b7a608bfbc62ca337e697951c0425323b77
Sean Watkins <slwatkins@uh.edu>
2018-01-18 11:18:58 -0600

    Update dependencies for Avalon 6.3

--- install_scripts/fedora4.sh
+++ install_scripts/fedora4.sh
@@ -10,16 +10,16 @@ if [ -f "$SHARED_DIR/install_scripts/config" ]; then
   . $SHARED_DIR/install_scripts/config
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/fcrepo-webapp-4.7.1.war" ]; then
+if [ ! -f "$DOWNLOAD_DIR/fcrepo-webapp-4.7.4.war" ]; then
   echo -n "Downloading Fedora 4..."
-  wget -q "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.7.1/fcrepo-webapp-4.7.1.war" -O "$DOWNLOAD_DIR/fcrepo-webapp-4.7.1.war"
+  wget -q "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.7.4/fcrepo-webapp-4.7.4.war" -O "$DOWNLOAD_DIR/fcrepo-webapp-4.7.4.war"
   echo " done"
 fi
 
 mkdir -p /var/avalon/fedora
 chown tomcat7:tomcat7 /var/avalon/fedora
 
-cp -f $DOWNLOAD_DIR/fcrepo-webapp-4.7.1.war /var/lib/tomcat7/webapps/fedora4.war
+cp -f $DOWNLOAD_DIR/fcrepo-webapp-4.7.4.war /var/lib/tomcat7/webapps/fedora4.war
 cp -f $SHARED_DIR/config/server.xml /etc/tomcat7/server.xml
 cp -f $SHARED_DIR/config/tomcat-users.xml /etc/tomcat7/tomcat-users.xml
 
--- install_scripts/passenger.sh
+++ install_scripts/passenger.sh
@@ -15,8 +15,8 @@ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/avalon/.rvm/scripts/rvm
 
-rvm install 2.2.5
-rvm  --default use 2.2.5
+rvm install 2.4.1
+rvm  --default use 2.4.1
 
 echo "Installing passenger..."
 gem install passenger -v 5.1.3 -q
